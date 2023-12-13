b7468d36963d134f6cf431686bca105936b389b2
NicolasPerezUNLaSMN <nykolaiperez@gmail.com>
2023-12-13 15:06:29 -0300

    Actualizados todos los torneos nov 2023

--- nav.js
+++ nav.js
@@ -45,6 +45,7 @@ document.write(`
               
               <li class="dropdown"><a href="#"><span>Categoria A</span> <i class="bi bi-chevron-right"></i></a>
                 <ul>
+	          <li><a href="a-nov-2023.html">Torneo Noviembre 2023</a></li>
                   <li><a href="a-agos-2023.html">Torneo Agosto 2023</a></li>
                   <li><a href="a-dic-2022.html">Torneo fin de a&ntildeo 2022</a></li>
                   <li><a href="a-dic-2022-2da.html">Torneo fin de a&ntildeo 2022 - 2da</a></li>
@@ -54,6 +55,8 @@ document.write(`
               
               <li class="dropdown"><a href="#"><span>Categoria B</span> <i class="bi bi-chevron-right"></i></a>
                 <ul>
+		  <li><a href="b-nov-2023.html">Torneo Noviembre 2023</a></li>
+		  <li><a href="b2-nov-2023.html">Torneo B2 Noviembre 2023</a></li>
                   <li><a href="b-agos-2023.html">Torneo Agosto 2023</a></li>
                   <li><a href="b-mar-2023.html">Torneo Marzo 2023</a></li>
                   <li><a href="b-mar-2023-2da.html">Torneo Marzo 2023 - 2da</a></li>
@@ -65,7 +68,8 @@ document.write(`
 
                 <li class="dropdown"><a href="#"><span>Categoria C</span> <i class="bi bi-chevron-right"></i></a>
                 <ul>
-		    <li><a href="c-nov-2023.html">Torneo Noviembre 2023</a></li>
+		  <li><a href="c-nov-2023.html">Torneo Noviembre 2023</a></li>
+                  <li><a href="c-nov-2023-2da.html">Torneo Noviembre 2023 - 2da</a></li>
  		  <li><a href="c-agos-2023-2da.html">Torneo Agosto 2023 -2da</a></li>
                   <li><a href="c-agos-2023.html">Torneo Agosto 2023</a></li>
 		  <li><a href="c-mar-2023.html">Torneo Marzo 2023</a></li>
